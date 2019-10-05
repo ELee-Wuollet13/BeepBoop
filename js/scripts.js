30e43f316791c487b9cb3a7ff809af8f29340f15
Everett Lee-Wuollet <everett.leewuollet@gmail.com>
2019-10-04 22:13:56 -0700

    continue work on making all nubers including 1, 2 or 3 change to text

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,12 @@
 function glitch(n) {
   var result = [];
-  for (i = 1; i <= n; i++) {
-    if (i === 3) {
+  console.log(result);
+  for (var i = 1; i <= n; i++) {
+    if (i.includes(3)) {
       result.push("I'm sorry dave, I'm affraid I can't do that");
-    } else if (i === 2) {
+    } else if (i.includes(2)) {
       result.push("Boop");
-    } else if (i === 1) {
+    } else if (i.includes(1)) {
       result.push("Beep");
     } else {
       result.push(i);
@@ -14,11 +15,10 @@ function glitch(n) {
   return result;
 }
 
-
 $(document).ready(function() {
   $("#input form").submit(function(event) {
     event.preventDefault();
-    var n = ($('input#intake').val());
+    var n = ('input#intake').val();
     console.log(n);
     var result = glitch(n);
     console.log(result);
