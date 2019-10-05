c27c9547ce54ded70fe2365029740f1c68652846
Everett Lee-Wuollet <everett.leewuollet@gmail.com>
2019-10-04 22:33:43 -0700

    fill array with all proper outputs including numbers and phrases

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,25 @@
 function glitch(n) {
   var result = [];
-  console.log(result);
   for (var i = 1; i <= n; i++) {
-    if (i.includes(3)) {
+    if (i.toString().includes("3")) {
       result.push("I'm sorry dave, I'm affraid I can't do that");
-    } else if (i.includes(2)) {
+    } else if (i.toString().includes("2")) {
       result.push("Boop");
-    } else if (i.includes(1)) {
+    } else if (i.toString().includes("1")) {
       result.push("Beep");
     } else {
       result.push(i);
     }
   }
+  console.log(i);
+  console.log(result);
   return result;
 }
 
 $(document).ready(function() {
   $("#input form").submit(function(event) {
     event.preventDefault();
-    var n = ('input#intake').val();
+    var n = parseInt($('input#intake').val());
     console.log(n);
     var result = glitch(n);
     console.log(result);
