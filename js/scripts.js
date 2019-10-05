df2b3cb83eca2d4dd087bfefddca365ab5d6c7e9
Everett Lee-Wuollet <everett.leewuollet@gmail.com>
2019-10-04 20:57:34 -0700

    establish basic functionality but intaking number from use and assigning a variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-  $(form).submit(function(event) {
+  $("#input form").submit(function(event) {
     event.preventDefault();
-    var n = $(parseInt(input#intake).val())
+    var n = parseInt($('input#intake').val());
     console.log(n);
   })
 })
