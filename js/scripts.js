901fab5cf50a46517551835c144eadc8815eac32
Everett Lee-Wuollet <everett.leewuollet@gmail.com>
2019-10-04 21:51:26 -0700

    complete creation of array of all numbers from 1 to use input with some numbers changed to desired words

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,24 @@
 function glitch(n) {
   var result = [];
-  for (var i = 1; i <= n; i++) {
-    if (i.includes(3)) {
+  for (i = 1; i <= n; i++) {
+    if (i === 3) {
       result.push("I'm sorry dave, I'm affraid I can't do that");
-    } else if (i.includes(2)) {
+    } else if (i === 2) {
       result.push("Boop");
-    } else if (i.includes(1)) {
+    } else if (i === 1) {
       result.push("Beep");
     } else {
       result.push(i);
     }
-    return result;
   }
+  return result;
 }
 
 
 $(document).ready(function() {
   $("#input form").submit(function(event) {
     event.preventDefault();
-    var n = parseInt($('input#intake').val());
+    var n = ($('input#intake').val());
     console.log(n);
     var result = glitch(n);
     console.log(result);
